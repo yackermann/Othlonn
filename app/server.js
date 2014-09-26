904c0849baa979fc56942c10bc25ce105070b90e
Niemand <yurii.akkermanov@gmail.com>
2014-09-26 21:11:38 +1200

    Callbacks

--- app/server.js
+++ app/server.js
@@ -1,7 +1,9 @@
-var http = require("http");
+function say(word){
+	console.log(word)
+}
 
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World!");
-  response.end();
-}).listen(3000);
+function execute(func, value){
+	func(value)
+}
+
+execute(say,'Hello World!')
